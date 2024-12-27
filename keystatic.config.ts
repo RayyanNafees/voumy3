2d204fef93ab95c9ff959383d150994491185369
9node <rayyan@theninthnode.com>
2024-12-27 18:34:55 +0530

    ♻️ 😎 refactor: Added Layouts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,10 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'cloud',
+  },
+  cloud: {
+    project: 'rayyan/voumy3',
   },
   collections: {
     posts: collection({
