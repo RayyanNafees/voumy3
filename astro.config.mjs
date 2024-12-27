d4238a814bbdd0d546977791422c17f71034db88
9node <rayyan@theninthnode.com>
2024-12-27 11:27:45 +0530

    :tada: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
+import vercel from "@astrojs/vercel";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		markdoc(),
+		keystatic(),
+		tailwind(),
+		preact({ compat: true, devtools: true }),
+		alpinejs(),
+	],
+
+	output: "static",
+	adapter: vercel(),
+});
